03d9be1f0ffedaad9aa74da98a03bc418bf74d0e
Philip Southam <philip.southam@jpl.nasa.gov>
2020-03-25 11:23:55 -0700

    adjusting jvm parameters

--- run-tika.sh
+++ run-tika.sh
@@ -11,4 +11,4 @@ for f in $(ls ${CTAKES_HOME}/lib/*.jar); do
 done
 
 
-java -classpath /srv/ctakes-config:${CTAKES_HOME}/lib/opennlp-tools-1.5.3.jar:/srv/tika-server-1.24.jar:${CTAKES_HOME}/desc:${CTAKES_HOME}/resources:${GENERATED_CLASSPATH} org.apache.tika.server.TikaServerCli -h 0.0.0.0 --config /srv/ctakes-config/tika-config.xml $@
+java -server -Xmx22g -XX:+TieredCompilation -classpath /srv/ctakes-config:${CTAKES_HOME}/lib/opennlp-tools-1.5.3.jar:/srv/tika-server-1.24.jar:${CTAKES_HOME}/desc:${CTAKES_HOME}/resources:${GENERATED_CLASSPATH} org.apache.tika.server.TikaServerCli -h 0.0.0.0 --config /srv/ctakes-config/tika-config.xml $@
